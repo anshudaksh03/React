359ec864990bed0370bd89554bc98aa1522803a2
Anshu Kumar <anshu123@gmail.com>
2025-08-19 06:29:45 +0530

    custom elements

---
+++ customReact/customreact.js
@@ -0,0 +1,17 @@
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+}
+
+const { Children } = require("react")
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    Children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+customRender(reactElement, mainContainer)
