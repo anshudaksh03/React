8310eb27693529c056874db0b64f9648876e7e82
Anshu Kumar <anshu123@gmail.com>
2025-08-20 06:45:53 +0530

    new method

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+ function customRender(reactElement, container){
+   /* const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href' , reactElement.props.href)
+    domElement.setAttribute('target' , reactElement.props.target)
+
+    container.appendChild(domElement) */
+
+    //---------------------------
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+       
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+} 
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'> vusut google</a>
+)
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <MyApp />   
+  anotherElement
 )
