8bab5d4e190539c3a7bae4b6a43dff4124fd4abc
Anshu Kumar <anshu123@gmail.com>
2025-08-22 04:02:48 +0530

    react make

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const username = "Anshu daksh"
 function MyApp() {
   return (
     <div>
-      <h1>Custom App !</h1>
+      <h1>Custom App ! {username}</h1>
     </div>
   )
 }
 
- function customRender(reactElement, container){
+ //function customRender(reactElement, container){
    /* const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href' , reactElement.props.href)
@@ -19,26 +20,32 @@ function MyApp() {
     container.appendChild(domElement) */
 
     //---------------------------
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    for (const prop in reactElement.props) {
+//     const domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML = reactElement.children
+//     for (const prop in reactElement.props) {
        
-        if (prop === 'children') continue;
-        domElement.setAttribute(prop, reactElement.props[prop])
-    }
-    container.appendChild(domElement)
-} 
+//         if (prop === 'children') continue;
+//         domElement.setAttribute(prop, reactElement.props[prop])
+//     }
+//     container.appendChild(domElement)
+// } 
 
 
 const anotherElement = (
   <a href="https://google.com" target='_blank'> vusut google</a>
 )
 
+const anotheruser = "Anshu prajapati"
 
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'}, 'click me to visit google', anotheruser
+)
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <MyApp />   
-  anotherElement
+  //<MyApp />   
+  reactElement
+ 
 )
